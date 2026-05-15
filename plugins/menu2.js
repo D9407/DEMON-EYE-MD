6f150e9c2eaa5d59b521c8e92ccbf3faf72a389d
Kingtad <darkstatehacker@gmail.com>
2026-05-15 12:18:26 +0200

    Add files via upload

---
+++ plugins/menu2.js
@@ -0,0 +1,107 @@
+/*created by King85 🕵
+contact dev1 263781315055 ♻️
+contact dev2 https://t.me/demoeyehacker ♻️
+© just give credit will you ⚠
+*/
+
+
+
+
+
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+    pattern: "menu2",
+    react: "👾",
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
+};
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `*┋* ${commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = `*╭─────────────────❒⁠⁠⁠⁠*
+
+*⇆ ʜɪ ᴍʏ ᴅᴇᴀʀ ғʀɪᴇɴᴅ ⇆*
+
+     *${pushname}*
+
+┏━━━━━━━━━━━━━━━━━━━━
+   *Welcome to DEMON-MD 2nd Menu*
+┗━━━━━━━━━━━━━━━━━━━━
+
+*Created by King85 👑 👨🏻‍💻*
+
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂ DOWNLOAD CMDS❂*
+*┕───────────────❒*
+*╭──────────●●►*
+${menu.download}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂MAIN CMDS❂*
+*┕───────────────❒*
+*╭──────────●●►*
+${menu.main}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂GROUP CMDS❂*
+*┕───────────────❒*
+
+*╭──────────●●►*
+${menu.group}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂OWNER CMDS❂*
+*┕───────────────❒*
+
+*╭──────────●●►*
+${menu.owner}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂CONVERTER CMDS❂*
+*┕───────────────❒*
+
+*╭──────────●●►*
+${menu.convert}
+*╰──────────●●►*
+
+*╭─────────────────❒⁠⁠⁠⁠*
+*│* *❂SEARCH CMDS ❂*
+*┕─────────────────❒*
+
+*╭──────────●●►*
+${menu.search}
+*╰──────────●●►*
+
+
+> *Powered by DEMON-MD*
+
+`;
+
+await conn.sendMessage(from,{image:{url:config.MENU_IMG},caption:madeMenu},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
